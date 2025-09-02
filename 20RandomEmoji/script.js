73fb21a9caa666128713796d1cf610a0b1a6e42a
Akhilkoppula <Akhilkoppula.henry4@gmail.com>
2025-09-02 18:28:27 +0530

    Random Emoji Generator

---
+++ 20RandomEmoji/script.js
@@ -0,0 +1,33 @@
+const btn=document.getElementById('emoji');
+const emojis=["😀",
+"😃",
+"😁",
+"😄",
+"😆",
+"😅",
+"😂",
+"🤣",
+"🥲",
+"🥹",
+"☺️",
+"😊",
+"😇",
+"🙂",
+"🙃",
+"😉",
+"😌",
+"😍",
+"🥰",
+"😘",
+"😗",
+"😙",
+"😚",
+"😋",
+"😛",
+"😝",
+"😜",
+"🤪"]
+
+btn.addEventListener("mouseover",()=>{
+    btn.innerHTML=emojis[Math.floor(Math.random()*emojis.length)];
+})
