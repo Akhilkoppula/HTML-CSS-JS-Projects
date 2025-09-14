e2d1c8dabaa0ec1ad02f9efa3fbf1a9e045abf1f
Akhilkoppula <Akhilkoppula.henry4@gmail.com>
2025-09-14 16:11:21 +0530

    Social Media Select Menu

---
+++ 28 Select Menu/script.js
@@ -0,0 +1,18 @@
+let selectFiled=document.getElementById('selectField')
+let selectText=document.getElementById('selectText')
+let list=document.getElementById('list')
+let arrowIcon=document.getElementById('arrowIcon')
+let options=document.getElementsByClassName('options')
+
+selectFiled.addEventListener("click",()=>{
+    list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
+});
+
+ for(option of options){
+      option.onclick=function(){
+        selectText.innerHTML=this.textContent;
+        list.classList.toggle("hide")
+        arrowIcon.classList.toggle('rotate')
+      }
+ }
